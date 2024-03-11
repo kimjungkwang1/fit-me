89878ee6e8ae02283d26201eea32495871df2cb7
Cha Cha <cha3088@gmail.com>
2024-03-11 11:04:28 +0900

    chore : initialize project

---
+++ back/src/main/java/site/chachacha/fitme/FitmeApplication.java
@@ -0,0 +1,13 @@
+package site.chachacha.fitme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/site/chachacha/fitme/FitmeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.chachacha.fitme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
