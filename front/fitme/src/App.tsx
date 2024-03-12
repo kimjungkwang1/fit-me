bdea7aac7f5ec28340429d3a64a32ae035e9cc53
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-03-12 17:52:58 +0900

    Feat: 카테고리 필터 모달 구현 (S10P22A306-99)

--- front/fitme/src/App.tsx
+++ front/fitme/src/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
-import { Button } from 'flowbite-react';
-=======
->>>>>>> 6c6aca413ea9bd1a2af4bf1b508560063ec48328
 import Test from './pages/test';
 
 function App() {
