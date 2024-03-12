3fc7b8c546392c0ddf2d14ee7552607b1ec90a4f
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-03-12 17:52:58 +0900

    Feat: 카테고리 필터 모달 구현 (S10P22A306-99)

--- front/fitme/src/components/Common/FilterBar.tsx
+++ front/fitme/src/components/Common/FilterBar.tsx
@@ -1,21 +1,88 @@
-import { Button } from 'flowbite-react';
-import React from 'react';
+import { Button, Modal } from 'flowbite-react';
+import React, { useState } from 'react';
 
 export default function FilterBar() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className='flex flex-row gap-1 mx-[3%] mt-2 mb-1'>
-      <Button color='light' size='xs'>
+      <Button color='light' size='xs' onClick={() => setOpenModal(true)}>
         브랜드
       </Button>
-      <Button color='light' size='xs'>
-        색상
-      </Button>
-      <Button color='light' size='xs'>
+      <Button color='light' size='xs' onClick={() => setOpenModal(true)}>
         카테고리
       </Button>
-      <Button color='light' size='xs'>
+      <Button color='light' size='xs' onClick={() => setOpenModal(true)}>
         가격
       </Button>
+      <Modal show={openModal} popup>
+        <Modal.Body>
+          <div>
+            <span className='flex font-semibold mt-2 mb-1'>브랜드</span>
+            <div className='flex flex-row flex-wrap gap-2'>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+              <Button size='xs' color='light'>
+                브랜드이름
+              </Button>
+            </div>
+          </div>
+          <div>
+            <span className='flex font-semibold mt-2 mb-1'>카테고리</span>
+            <div className='flex flex-row flex-wrap gap-2'>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+              <Button size='xs' color='light'>
+                카테고리이름
+              </Button>
+            </div>
+          </div>
+          <div>
+            <span className='flex font-semibold mt-2 mb-1'>가격</span>
+          </div>
+          <Button size='sm' onClick={() => setOpenModal(false)}>
+            적용
+          </Button>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
