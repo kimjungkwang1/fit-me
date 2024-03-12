c8adf542c2794339d6da5622711c39ad76001214
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-03-12 15:32:08 +0900

    Feat: 카테고리 정렬 기준 드롭다운 구현 (S10P22A306-99)

--- front/fitme/src/pages/test.tsx
+++ front/fitme/src/pages/test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // TestPage에 import하고 싶은 페이지 경로를 적어주세요.
-import TestPage from '../pages/login';
+import CategoryPage from './CategoryPage';
 
 const test: React.FC = () => {
   return (
     <>
-      <TestPage></TestPage>
+      <CategoryPage />
     </>
   );
 };
