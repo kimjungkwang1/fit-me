ff2b0c0d5c15432c0ec91f3d74e9d6fdf1a19cd4
card0923 <card0293@naver.com>
2024-03-14 14:08:43 +0900

    Feat: 피드 작성 화면 (S10P22A306-119)

---
+++ front/fitme/src/pages/FeedWritePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import User from '../components/Feed/User';
+import FeedPostForm from '../components/Feed/FeedPostForm';
+export default function FeedWritePage() {
+  return (
+    <>
+      <User></User>
+      <FeedPostForm></FeedPostForm>
+    </>
+  );
+}
