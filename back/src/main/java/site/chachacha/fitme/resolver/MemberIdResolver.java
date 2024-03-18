3db2ba40f3b78136990071ddf124cf40b35380ba
이상영 <syxxng25@gmail.com>
2024-03-18 14:17:36 +0900

    Feat: memberId 추출 위치 변경 (#S10P22A306-203)

--- back/src/main/java/site/chachacha/fitme/resolver/MemberIdResolver.java
+++ back/src/main/java/site/chachacha/fitme/resolver/MemberIdResolver.java
@@ -7,8 +7,16 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import site.chachacha.fitme.annotation.MemberId;
+import site.chachacha.fitme.auth.service.JwtService;
 
 public class MemberIdResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtService jwtService;
+
+    public MemberIdResolver(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().equals(Long.class) && parameter.hasParameterAnnotation(
@@ -16,9 +24,13 @@ public class MemberIdResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-
-        return request.getAttribute("memberId");
+        String accessToken = request.getHeader("Authorization");
+        if (accessToken == null || accessToken.isBlank()) {
+            return null;
+        }
+        return jwtService.validateAndExtractMemberIdFromAccessToken(accessToken);
     }
 }
