22c7bebdde5d078e71ecff73c139e219ef52db82
Cha Cha <cha3088@gmail.com>
2024-03-18 15:17:34 +0900

    Fix : 빌드 시 테스트 오류 해결

--- back/src/test/java/site/chachacha/fitme/config/EmbeddedRedisConfig.java
+++ back/src/test/java/site/chachacha/fitme/config/EmbeddedRedisConfig.java
@@ -2,21 +2,31 @@ package site.chachacha.fitme.config;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import redis.embedded.RedisServer;
 
+@Slf4j
 @Configuration
 public class EmbeddedRedisConfig {
     private final RedisServer redisServer;
 
-    public EmbeddedRedisConfig(@Value("${spring.redis.port}") int port) {
-        this.redisServer = new RedisServer(port);
+    public EmbeddedRedisConfig(@Value("${spring.redis.port}") int port, @Value("${spring.redis.maxmemory}") String maxMemory) {
+        this.redisServer = RedisServer.builder()
+            .port(port)
+            .setting("maxmemory " + maxMemory + "M")
+            .build();
     }
 
     @PostConstruct
     public void startRedis() {
-        this.redisServer.start();
+        try {
+            this.redisServer.start();
+        }
+        catch (Exception e) {
+            log.error("Embedded Redis Server Start Fail", e);
+        }
     }
 
     @PreDestroy
