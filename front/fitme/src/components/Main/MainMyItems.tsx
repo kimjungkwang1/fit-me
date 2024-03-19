26c8f226d4d653f0b287aaa752dd36e443b08946
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-03-19 17:34:00 +0900

    Feat: 메인페이지 api 연결 (S10P22A306-179)

--- front/fitme/src/components/Main/MainMyItems.tsx
+++ front/fitme/src/components/Main/MainMyItems.tsx
@@ -6,13 +6,13 @@ export default function MainMyItems() {
     <>
       <p className='text-start mx-[3%] mt-2 mb-1'>사용자 맞춤 추천</p>
       <div className='overflow-x-scroll flex items-start mx-[3%] space-x-[1.4%]'>
+        {/* <Item />
         <Item />
         <Item />
         <Item />
         <Item />
         <Item />
-        <Item />
-        <Item />
+        <Item /> */}
       </div>
     </>
   );
