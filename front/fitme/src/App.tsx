537ae9bde394eacd61ab02c78be3adcaf4c2a734
Siyeon <nova0109@naver.com>
2024-03-19 14:03:01 +0900

    Feat: 전체 페이지 css 수정

--- front/fitme/src/App.tsx
+++ front/fitme/src/App.tsx
@@ -13,36 +13,38 @@ import FeedPage from './pages/FeedPage';
 import FeedDetailPage from './pages/FeedDetailPage';
 import MyFeedPage from './pages/MyFeedPage';
 import FeedWritePage from './pages/FeedWritePage';
-// import HeaderBar from './components/headerBar';
-// import FooterBar from './components/footerBar';
+import HeaderBar from './components/headerBar';
+import FooterBar from './components/footerBar';
 
 function App() {
   return (
     <>
       <div className='flex justify-center'>
-        <div className='aspect-iphone h-screen'>
-          <Routes>
-            {/* Test 페이지 남겨뒀습니다 */}
-            <Route path='/test' element={<Test />} />
-            <Route path='/' element={<MainPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/category' element={<CategoryPage />} />
-            <Route path='/detail/:item_code' element={<ItemDetailPage />} />
-            <Route path='/search' element={<SearchPage />} />
-            <Route path='/cart' element={<CartPage />} />
-            {/* /payment/complete 페이지 아직 없음 */}
-            <Route path='/payment/complete' element={<LoginPage />} />
-            <Route path='/dressroom' element={<DressroomPage />} />
-            <Route path='/mypage' element={<MyPage />} />
-            <Route path='/feed' element={<FeedPage />} />
-            <Route path='/feed/:feed_no' element={<FeedDetailPage />} />
-            <Route path='/feed/myfeed' element={<MyFeedPage />} />
-            <Route path='/feed/write' element={<FeedWritePage />} />
-          </Routes>
-
-          {/* <HeaderBar /> */}
-          {/* <FooterBar /> */}
+        <div className='aspect-iphone h-screen select-none flex flex-col '>
+          <HeaderBar />
+          <div className='flex-grow overflow-auto hide-scrollbar'>
+            <Routes>
+              {/* Test 페이지 남겨뒀습니다 */}
+              <Route path='/test' element={<Test />} />
+              <Route path='/' element={<MainPage />} />
+              <Route path='/home' element={<MainPage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/signup' element={<SignupPage />} />
+              <Route path='/category' element={<CategoryPage />} />
+              <Route path='/detail/:item_code' element={<ItemDetailPage />} />
+              <Route path='/search' element={<SearchPage />} />
+              <Route path='/cart' element={<CartPage />} />
+              {/* /payment/complete 페이지 아직 없음 */}
+              <Route path='/payment/complete' element={<LoginPage />} />
+              <Route path='/dressroom' element={<DressroomPage />} />
+              <Route path='/mypage' element={<MyPage />} />
+              <Route path='/feed' element={<FeedPage />} />
+              <Route path='/feed/:feed_no' element={<FeedDetailPage />} />
+              <Route path='/feed/myfeed' element={<MyFeedPage />} />
+              <Route path='/feed/write' element={<FeedWritePage />} />
+            </Routes>
+          </div>
+          <FooterBar />
         </div>
       </div>
     </>
