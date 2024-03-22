1fca84066b126552112308716de870cc10f03115
card0923 <card0293@naver.com>
2024-03-22 14:12:26 +0900

    chore/button

--- front/fitme/src/components/dressroom/DressroomMyFittingShow.tsx
+++ front/fitme/src/components/dressroom/DressroomMyFittingShow.tsx
@@ -5,7 +5,7 @@ export default function DressroomMyfittingShow() {
     <>
       <div className='relative my-2'>
         <DressroomMyfitting></DressroomMyfitting>
-        <button className='absolute top-4 right-[-10px] p-2 bg-red-500 text-white opacity-100 rounded-full w-10 h-10'>
+        <button className='absolute top-4 right-[-10px] p-2 bg-red-500 text-white opacity-100 rounded-full w-10 h-10 z-10'>
           X
         </button>
       </div>
