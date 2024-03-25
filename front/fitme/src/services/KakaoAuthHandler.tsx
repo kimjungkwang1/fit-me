41b3d3b078abb5ab52f80b2414fa56a5f4a5c388
Siyeon <nova0109@naver.com>
2024-03-25 15:28:37 +0900

    Feat: 로그인 api 수정1 (S10P22A306-176)

--- front/fitme/src/services/KakaoAuthHandler.tsx
+++ front/fitme/src/services/KakaoAuthHandler.tsx
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const KakaoAuthHandler: React.FC = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -10,8 +12,24 @@ const KakaoAuthHandler: React.FC = () => {
 
     if (code) {
       console.log('받은 코드:', code);
+      axios
+        .get('https://fit-me.site/api/auth/login/oauth2/code/kakao' + code)
+        .then((res) => {
+          if (res.status === 200) {
+            let accessToken = res.headers.Authorization;
+            let refreshToken = res.headers.AuthorizationRefresh;
+            console.log('refresh 토큰 :', refreshToken);
+            console.log('access 토큰 :', accessToken);
+            console.log('로그인 성공');
+            navigate('/signup');
+          } else {
+            alert('로그인 오류');
+            navigate('/home');
+          }
+        })
+        .catch((error) => console.log(error));
     }
-  }, [location]);
+  }, [location, navigate]);
 
   return <div>카카오 로그인 처리 중...</div>;
 };
