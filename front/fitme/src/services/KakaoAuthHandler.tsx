5fb7cb18a2a083e12b436e11d7174a32c3b8e9fe
Siyeon <nova0109@naver.com>
2024-03-26 10:56:05 +0900

    Fix: 메인 페이지 수정 및 로그인 수정
    Former-commit-id: ddc2be1bd052b424a3de9836a5d1530bf317cb23

--- front/fitme/src/services/KakaoAuthHandler.tsx
+++ front/fitme/src/services/KakaoAuthHandler.tsx
@@ -5,32 +5,56 @@ import { useLocation, useNavigate } from 'react-router-dom';
 const KakaoAuthHandler: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  let count = 0;
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const code = searchParams.get('code');
+    if (count === 0) {
+      count++;
+      console.log(count);
+      const searchParams = new URLSearchParams(location.search);
+      console.log('여기까지 1');
+      const code = searchParams.get('code');
+      console.log('여기까지 2');
 
-    if (code) {
-      console.log('받은 코드:', code);
-      axios
-        .get('https://fit-me.site/api/auth/login/oauth2/code/kakao?code=' + code)
-        .then((res) => {
-          console.log(res.status);
-          if (res.status === 200) {
-            let accessToken = res.headers.Authorization;
-            let refreshToken = res.headers.AuthorizationRefresh;
-            console.log('refresh 토큰 :', refreshToken);
-            console.log('access 토큰 :', accessToken);
-            console.log('로그인 성공');
-            navigate('/signup');
-          } else {
-            alert('로그인 오류');
-            navigate('/home');
-          }
-        })
-        .catch((error) => console.log(error));
+      if (code) {
+        console.log('여기까지 3');
+
+        console.log('받은 코드:', code);
+        const AXIOS_URL = 'https://fit-me.site/api/auth/login/oauth2/code/kakao?code=' + code;
+        console.log(AXIOS_URL);
+
+        console.log('여기까지 4');
+
+        axios
+          .get(AXIOS_URL)
+          .then((res) => {
+            console.log(res.status);
+            console.log('여기까지 5');
+
+            if (res.status === 200) {
+              console.log('여기까지 왔다');
+              let accessToken = res.headers.Authorization;
+              let refreshToken = res.headers.AuthorizationRefresh;
+              console.log('refresh 토큰 :', refreshToken);
+              console.log('access 토큰 :', accessToken);
+              console.log('로그인 성공');
+              navigate('/signup');
+            } else {
+              alert('로그인 오류');
+              navigate('/home');
+            }
+          })
+          .catch((error) => {
+            console.log('여기까지 6');
+            console.log('안돼');
+
+            console.log(error);
+          });
+      }
+
+      console.log('끝까지 왔다');
     }
-  }, [location, navigate]);
+  }, [location]);
 
   return <div>카카오 로그인 처리 중...</div>;
 };
