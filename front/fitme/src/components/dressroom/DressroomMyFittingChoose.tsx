ed07b0c1e33d09701d863c0c0a8343be9047aa1e
card0923 <card0293@naver.com>
2024-03-27 14:13:31 +0900

    Feat : axios api

--- front/fitme/src/components/dressroom/DressroomMyFittingChoose.tsx
+++ front/fitme/src/components/dressroom/DressroomMyFittingChoose.tsx
@@ -7,7 +7,7 @@ export default function DressroomMyFittingChoose() {
     <div>
       <div className='relative my-2'>
         <DressroomMyfitting></DressroomMyfitting>
-        <FaCheck className='absolute top-4 right-[-10px] p-2 bg-green-500 text-white opacity-100 rounded-full w-10 h-10 z-10'></FaCheck>
+        <FaCheck className='absolute top-4 right-[-4px] p-2 bg-green-500 text-white opacity-100 rounded-full w-8 h-8 z-10'></FaCheck>
       </div>
     </div>
   );
