461e274822916566a43cd42bcf500ffb57bffe75
Siyeon <nova0109@naver.com>
2024-03-27 18:58:52 +0900

    Feat: 로그아웃 구현
    Former-commit-id: 6f11a83c350c07a1d3afff2e449b4b36f4c94090

--- front/fitme/src/components/Main/MainAdv.tsx
+++ front/fitme/src/components/Main/MainAdv.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Carousel } from 'flowbite-react';
 import adv01 from '../../assets/images/adv/adv01.png';
 import adv02 from '../../assets/images/adv/adv02.png';
+import adv03 from '../../assets/images/adv/adv03.png';
+import adv04 from '../../assets/images/adv/adv04.png';
 
 export default function MainAdv() {
   return (
@@ -9,6 +11,8 @@ export default function MainAdv() {
       <Carousel className='h-80'>
         <img src={adv01} alt='advertisement01' />
         <img src={adv02} alt='advertisement02' />
+        <img src={adv03} alt='advertisement03' />
+        <img src={adv04} alt='advertisement04' />
       </Carousel>
     </div>
   );
