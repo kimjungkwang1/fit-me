86af3fd24af966c820a8b9fa921ff32e75b0a5ba
seonghyeon.moon <seonghyeon.moon@snu.ac.kr>
2024-03-27 13:24:29 +0900

    Feat: 선택된 옵션 삭제 기능 추가 (S10P22A306-238)

--- front/fitme/src/components/Category/CategoryItemList.tsx
+++ front/fitme/src/components/Category/CategoryItemList.tsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Item from '../Common/Item';
+import axios from 'axios';
+
+type ImageType = {
+  id: number;
+  url: string;
+};
+
+type BrandType = {
+  id: number;
+  name: string;
+};
+
+type ItemType = {
+  id: number;
+  name: string;
+  price: number;
+  mainImages: ImageType[];
+  brand: BrandType;
+  likeCount: number;
+  reviewRating: number;
+  reviewCount: number;
+};
 
 export default function CategoryItemList() {
+  const [list, setList] = useState<ItemType[]>([]);
+
+  useEffect(() => {
+    axios.get(`https://fit-me.site/api/products`).then(({ data }) => {
+      setList(data);
+    });
+  }, []);
+
   return (
     <div>
       <div className='flex flex-wrap flex-row mx-[2%] place-content-around gap-y-3'>
-        {/* <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item /> */}
+        {list &&
+          list.map((item, index) => (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              price={item.price}
+              mainImages={item.mainImages}
+              brand={item.brand}
+              likeCount={item.likeCount}
+              reviewRating={item.reviewRating}
+              reviewCount={item.reviewCount}
+            />
+          ))}
       </div>
     </div>
   );
