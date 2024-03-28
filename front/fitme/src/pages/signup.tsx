ceee8f4edd580cfdab8873b3498c896ac11e1aa6
Siyeon <nova0109@naver.com>
2024-03-28 17:45:50 +0900

    Feat: 회원정보수정 및 정보입력(S10P22A306-195)
    Former-commit-id: 2ccd192ff7dbd50cdfbe32b2fb028235b0807055

--- front/fitme/src/pages/signup.tsx
+++ front/fitme/src/pages/signup.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { api } from '../services/api ';
+import { api } from '../services/api';
 import UserInput from '../components/user/userInput';
+
+type ApiDataType = {
+  id: number;
+  nickname: string;
+  gender: boolean;
+  profileUrl: string;
+  phoneNumber: string;
+  birthYear: number;
+  address: string;
+};
 
 const year = new Date().getFullYear();
 
-const signup: React.FC = () => {
-  const handleSubmit = async (formData: FormData) => {
-    try {
-      const response = await axios.post('여기에_서버_URL', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+const Signup: React.FC = () => {
+  const [apiData, setApiData] = useState<ApiDataType>();
+
+  useEffect(() => {
+    api
+      .get('https://fit-me.site/api/members')
+      .then((res) => {
+        setApiData(res.data);
+      })
+      .catch((error) => {
+        console.error('서버로부터 에러 응답:', error);
       });
+  });
+
+  const handleSubmit = async (apiData: ApiDataType) => {
+    try {
+      const response = await axios.post('https://fit-me.site/api/members////', apiData);
       console.log('서버 응답:', response.data);
     } catch (error) {
       console.error('서버로부터 에러 응답:', error);
     }
   };
-  return (
-    <>
-      <UserInput onSubmit={handleSubmit} />
-    </>
-  );
+
+  return <>{apiData && <UserInput onSubmit={handleSubmit} apiData={apiData} />} </>;
 };
 
-export default signup;
+export default Signup;
