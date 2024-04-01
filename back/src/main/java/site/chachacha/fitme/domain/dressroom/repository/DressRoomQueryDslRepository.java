a3ea3d35d7dd8f87583b9ef885a9c1457814027b
Cha Cha <cha3088@gmail.com>
2024-04-01 16:35:01 +0900

    Fix : Order API NoOffset OrderBy

--- back/src/main/java/site/chachacha/fitme/domain/dressroom/repository/DressRoomQueryDslRepository.java
+++ back/src/main/java/site/chachacha/fitme/domain/dressroom/repository/DressRoomQueryDslRepository.java
@@ -5,6 +5,11 @@ import java.util.Optional;
 import site.chachacha.fitme.domain.dressroom.entity.DressRoom;
 
 public interface DressRoomQueryDslRepository {
+
     List<DressRoom> findNoOffsetByMemberId(Long memberId, Long dressRoomId);
+
     Optional<DressRoom> findByIdAndMemberId(Long memberId, Long dressRoomId);
+
+    Optional<DressRoom> findByModelAndProductTopAndProductBottom(Long modelId, Long productTopId,
+        Long productBottomId);
 }
