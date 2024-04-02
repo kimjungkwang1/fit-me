d3c359504ac45fe09a7b45a4f3d0782ae8365731
cha3088 <cha3088@gmail.com>
2024-04-02 22:47:28 +0900

    Fix : ProductRecommendation API Query Fix

--- back/src/main/java/site/chachacha/fitme/domain/product/repository/ProductRecommendationRepositoryImpl.java
+++ back/src/main/java/site/chachacha/fitme/domain/product/repository/ProductRecommendationRepositoryImpl.java
@@ -16,7 +16,7 @@ public class ProductRecommendationRepositoryImpl implements
     @Override
     public List<ProductRecommendation> findByProductIdWithProduct(Long productId) {
         return queryFactory.selectFrom(productRecommendation)
-            .leftJoin(productRecommendation.product).fetchJoin()
+            .leftJoin(productRecommendation.recommendation).fetchJoin()
             .where(productRecommendation.product.id.eq(productId))
             .fetch();
     }
