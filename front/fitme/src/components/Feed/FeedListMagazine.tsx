2a265ac79cb21dd690904c5c873dfc1156d0656b
Siyeon <nova0109@naver.com>
2024-04-02 09:25:13 +0900

    Fix: 이미지 비율 변경
    Former-commit-id: d82d15be031b730cff8dbef13c32dbd6b563d544

--- front/fitme/src/components/Feed/FeedListMagazine.tsx
+++ front/fitme/src/components/Feed/FeedListMagazine.tsx
@@ -6,7 +6,7 @@ export default function FeedListMagazine() {
     <>
       <div className='grid grid-cols-3 flex-wrap justify-between mx-[2vw]'>
         {imageNumbers.map((num) => (
-          <div className='m-1 h-64 w-42 flex justify-center items-center  hover:scale-105 duration-200'>
+          <div className='m-1 aspect-[11/14] flex justify-center items-center  hover:scale-105 duration-300'>
             <img
               className='w-full h-full object-cover'
               src={`https://fit-me.site/images/feed/magazine/magazine${num}.png`}
--- front/fitme/src/components/Feed/FeedListUser.tsx
+++ front/fitme/src/components/Feed/FeedListUser.tsx
@@ -6,7 +6,7 @@ export default function FeedListUser() {
     <>
       <div className='grid grid-cols-3 justify-between mx-[2vw]'>
         {imageNumbers.map((num) => (
-          <div className='m-1 h-64 w-42 flex justify-center items-center rounded-md border-solid border-2 border-black hover:scale-105 duration-200'>
+          <div className='m-1 aspect-[7/10] flex justify-center items-center rounded-md border-solid border-2 border-black hover:scale-105 duration-300'>
             <img
               className='w-full h-full object-cover rounded-sm'
               src={`https://fit-me.site/images/feed/ootd/ootd${num}.png`}
