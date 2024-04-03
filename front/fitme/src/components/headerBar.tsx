0717b1e2694a1f9c0dbbf0eaed003a291adf4472
Siyeon <nova0109@naver.com>
2024-04-03 17:17:42 +0900

    Feat: 카테고리 페이지 수정

--- front/fitme/src/components/headerBar.tsx
+++ front/fitme/src/components/headerBar.tsx
@@ -41,24 +41,25 @@ const HeaderBar: React.FC = () => {
   let left, right, content;
   if (currentTab === 'mypage' || currentTab === 'home') {
     left = (
-      <button key='alertElement'>
-        <Tooltip content='알림'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-8 h-8'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0'
-            />
-          </svg>
-        </Tooltip>
-      </button>
+      <></>
+      // <button key='alertElement'>
+      //   <Tooltip content='알림'>
+      //     <svg
+      //       xmlns='http://www.w3.org/2000/svg'
+      //       fill='none'
+      //       viewBox='0 0 24 24'
+      //       strokeWidth={1.5}
+      //       stroke='currentColor'
+      //       className='w-8 h-8'
+      //     >
+      //       <path
+      //         strokeLinecap='round'
+      //         strokeLinejoin='round'
+      //         d='M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0'
+      //       />
+      //     </svg>
+      //   </Tooltip>
+      // </button>
     );
   } else if (currentTab === 'dressroom' || currentTab === 'feed' || currentTab === 'category') {
   } else {
@@ -86,7 +87,26 @@ const HeaderBar: React.FC = () => {
     let additionalElement;
     const basicElement = (
       <button onClick={() => navigate('/search')} key='basicElement'>
-        <Tooltip content='검색'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-8 h-8'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+          />
+        </svg>
+      </button>
+    );
+    if (currentTab === 'cart') {
+    } else if (currentTab === 'feed' || currentTab === 'feedDetail') {
+      additionalElement = (
+        <button onClick={() => navigate('/feed/myfeed')} key='additionalElement'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
@@ -98,53 +118,28 @@ const HeaderBar: React.FC = () => {
             <path
               strokeLinecap='round'
               strokeLinejoin='round'
-              d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+              d='M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
             />
           </svg>
-        </Tooltip>
-      </button>
-    );
-    if (currentTab === 'cart') {
-    } else if (currentTab === 'feed' || currentTab === 'feedDetail') {
-      additionalElement = (
-        <button onClick={() => navigate('/feed/myfeed')} key='additionalElement'>
-          <Tooltip content='나의 피드'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-8 h-8'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
-              />
-            </svg>
-          </Tooltip>
         </button>
       );
     } else {
       additionalElement = (
         <button onClick={() => navigate('/cart')} key='additionalElement'>
-          <Tooltip content='장바구니'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-8 h-8'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
-              />
-            </svg>
-          </Tooltip>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-8 h-8'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
+            />
+          </svg>
         </button>
       );
     }
