443c91980276cfd3ac3be95427196d14d936abdc
Cha Cha <cha3088@gmail.com>
2024-04-03 16:00:20 +0900

    Fix : DressRoom 로직 큰 수정
    Former-commit-id: 0306d44614749842d23c934cc65b62c0bcc3758d

--- back/src/main/java/site/chachacha/fitme/domain/dressroom/service/DressRoomService.java
+++ back/src/main/java/site/chachacha/fitme/domain/dressroom/service/DressRoomService.java
@@ -29,7 +29,6 @@ import site.chachacha.fitme.domain.dressroom.repository.DressRoomRepository;
 import site.chachacha.fitme.domain.dressroom.repository.ModelRepository;
 import site.chachacha.fitme.domain.member.entity.Member;
 import site.chachacha.fitme.domain.member.repository.MemberRepository;
-import site.chachacha.fitme.domain.product.entity.Product;
 import site.chachacha.fitme.domain.product.repository.ProductRepository;
 
 @Slf4j
@@ -66,155 +65,269 @@ public class DressRoomService {
             throw new BadRequestException("상의나 하의 중 하나는 선택해야 합니다.");
         }
 
+        // member가 존재하지 않으면 GoneException 발생
         Member member = memberRepository.findNotDeletedById(memberId)
             .orElseThrow(() -> new GoneException("존재하지 않는 회원입니다."));
 
+        // model이 존재하지 않으면 GoneException 발생
         Model model = modelRepository.findById(modelId)
             .orElseThrow(() -> new GoneException("존재하지 않는 모델입니다."));
 
-        Product top = null;
-        if (productTopId != null) {
-            top = productRepository.findById(productTopId)
-                .orElseThrow(() -> new GoneException("존재하지 않는 상의입니다."));
-        }
-
-        Product bottom = null;
-        if (productBottomId != null) {
-            bottom = productRepository.findById(productBottomId)
-                .orElseThrow(() -> new GoneException("존재하지 않는 하의입니다."));
-        }
-
-        Boolean topAlready = null;
-        Boolean bottomAlready = null;
-
-        // productTopId, productBottomId 둘 다 있는 경우
+        // 상, 하의 둘 다 요청이 들어온 경우
         if (productTopId != null && productBottomId != null) {
-            topAlready = !dressRoomRepository.existsByProductTopAndProductBottomIsNull(top);
-            bottomAlready = !dressRoomRepository.existsByProductBottomAndProductTopIsNull(
-                bottom);
+            // 상, 하의 이미 둘 다 드레스룸에 있는지 조회
+            List<DressRoom> dressRooms = dressRoomRepository.findByModelAndProductTopAndProductBottom(
+                modelId, productTopId, productBottomId);
+
+            // 이미 존재하는 경우
+            if (!dressRooms.isEmpty()) {
+                // 그대로 반환하면 돼
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRooms.get(0).getProductTop())
+                    .productBottom(dressRooms.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 이 저장 로직은 움직이지 마세요.
+                DressRoom savedDressRoom = dressRoomRepository.save(dressRoom);
+
+                return DressRoomResponse.of(savedDressRoom);
+            }
+
+            // 상의, 하의 동시에 입힌게 드레스룸에 없는 경우
+
+            // 상의 이미 드레스룸에 있는지 확인
+            List<DressRoom> dressRoomWithTop = dressRoomRepository.findByModelAndProductTopAndProductBottomIsNull(
+                modelId, productTopId);
+
+            // 상의 이미 캐싱되어 있는 경우
+            if (!dressRoomWithTop.isEmpty()) {
+                // 저장
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRoomWithTop.get(0).getProductTop())
+                    .productBottom(dressRoomWithTop.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 캐시된 상의를 기반으로 하의 추가
+                requestAIServer(modelId, productTopId, productBottomId, dressRoom, true, false);
+
+                return DressRoomResponse.of(dressRoom);
+            }
+            // 상의 캐싱되어 있지 않은 경우
+            else {
+                // 하의 이미 드레스룸에 있는지 확인
+                List<DressRoom> dressRoomWithBottom = dressRoomRepository.findByModelAndProductBottomAndProductTopIsNull(
+                    modelId, productBottomId);
+
+                // 하의 이미 캐싱되어 있는 경우
+                if (!dressRoomWithBottom.isEmpty()) {
+                    // 저장
+                    DressRoom dressRoom = DressRoom.builder()
+                        .model(model)
+                        .productTop(dressRoomWithBottom.get(0).getProductTop())
+                        .productBottom(dressRoomWithBottom.get(0).getProductBottom())
+                        .member(member)
+                        .build();
+
+                    // 캐시된 하의를 기반으로 상의 추가
+                    requestAIServer(modelId, productTopId, productBottomId, dressRoom, false, true);
+
+                    return DressRoomResponse.of(dressRoom);
+                }
+                // 하의 캐싱되어 있지 않은 경우
+                else {
+                    // 저장
+                    DressRoom dressRoom = DressRoom.builder()
+                        .model(model)
+                        .productTop(dressRooms.get(0).getProductTop())
+                        .productBottom(dressRooms.get(0).getProductBottom())
+                        .member(member)
+                        .build();
+
+                    // 캐시된 상의, 하의가 없는 경우
+                    requestAIServer(modelId, productTopId, productBottomId, dressRoom, false,
+                        false);
+
+                    return DressRoomResponse.of(dressRoom);
+                }
+            }
         }
-        // productTopId가 있는 경우
+        // 상의만 요청이 들어온 경우
         else if (productTopId != null) {
-            topAlready = !dressRoomRepository.existsByProductTopAndProductBottomIsNull(top);
+            // 상의만 드레스룸에 있는지 확인
+            List<DressRoom> dressRooms = dressRoomRepository.findByModelAndProductTopAndProductBottomIsNull(
+                modelId, productTopId);
+
+            // 상의만 있는 드레스룸이 이미 존재하는 경우
+            if (!dressRooms.isEmpty()) {
+                // 그대로 반환하면 돼
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRooms.get(0).getProductTop())
+                    .productBottom(dressRooms.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 이 저장 로직은 움직이지 마세요.
+                DressRoom savedDressRoom = dressRoomRepository.save(dressRoom);
+
+                return DressRoomResponse.of(savedDressRoom);
+            }
+            // 상의만 있는 드레스룸이 존재하지 않는 경우
+            else {
+                // 저장
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRooms.get(0).getProductTop())
+                    .productBottom(dressRooms.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 캐시된 상의가 없는 경우
+                requestAIServer(modelId, productTopId, productBottomId, dressRoom, false, null);
+
+                return DressRoomResponse.of(dressRoom);
+            }
         }
-        // productBottomId가 있는 경우
+        // 하의만 요청이 들어온 경우
         else if (productBottomId != null) {
-            bottomAlready = !dressRoomRepository.existsByProductBottomAndProductTopIsNull(
-                bottom);
+            // 하의만 드레스룸에 있는지 확인
+            List<DressRoom> dressRooms = dressRoomRepository.findByModelAndProductBottomAndProductTopIsNull(
+                modelId, productBottomId);
+
+            // 하의만 있는 드레스룸이 이미 존재하는 경우
+            if (!dressRooms.isEmpty()) {
+                // 그대로 반환하면 돼
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRooms.get(0).getProductTop())
+                    .productBottom(dressRooms.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 이 저장 로직은 움직이지 마세요.
+                DressRoom savedDressRoom = dressRoomRepository.save(dressRoom);
+
+                return DressRoomResponse.of(savedDressRoom);
+            }
+            // 하의만 있는 드레스룸이 존재하지 않는 경우
+            else {
+                // 저장
+                DressRoom dressRoom = DressRoom.builder()
+                    .model(model)
+                    .productTop(dressRooms.get(0).getProductTop())
+                    .productBottom(dressRooms.get(0).getProductBottom())
+                    .member(member)
+                    .build();
+
+                // 캐시된 하의가 없는 경우
+                requestAIServer(modelId, productTopId, productBottomId, dressRoom, null, false);
+
+                return DressRoomResponse.of(dressRoom);
+            }
         }
 
-        // 캐싱
-        // DressRoom Entity 중에 같은 Model, ProductTop, ProductBottom이 있는지 확인
-        List<DressRoom> existingDressRoom = dressRoomRepository.findByModelAndProductTopAndProductBottom(
-            modelId, productTopId, productBottomId);
+        // 이외의 경우
+        throw new BadRequestException("상의나 하의 중 하나는 선택해야 합니다.");
+    }
 
-        DressRoom dressRoom = DressRoom.builder()
-            .model(model)
-            .productTop(top)
-            .productBottom(bottom)
-            .member(member)
+    private void requestAIServer(Long modelId, Long productTopId, Long productBottomId,
+        DressRoom savedDressRoom, Boolean topAlready, Boolean bottomAlready) {
+        // AI 서버에 요청
+        WebClient webClient = WebClient.builder()
+            .baseUrl("http://222.107.238.75:8111")
             .build();
 
-        // 이 저장 로직은 움직이지 마세요.
-        DressRoom savedDressRoom = dressRoomRepository.save(dressRoom);
+        DressRoomAIRequest request = DressRoomAIRequest.builder()
+            .dressRoomId(savedDressRoom.getId())
+            .modelId(modelId)
+            .productTopId(productTopId)
+            .topAlready(topAlready)
+            .productBottomId(productBottomId)
+            .bottomAlready(bottomAlready)
+            .build();
 
-        // 존재하지 않으면 새로운 이미지 생성
-        if (existingDressRoom.isEmpty()) {
-            // AI 서버에 요청
-            WebClient webClient = WebClient.builder()
-                .baseUrl("http://222.107.238.75:8111")
-                .build();
+        log.info("dressRoomId: {}", request.getDressRoomId());
+        log.info("modelId: {}", request.getModelId());
+        log.info("productTopId: {}", request.getProductTopId());
+        log.info("topAlready: {}", request.getTopAlready());
+        log.info("productBottomId: {}", request.getProductBottomId());
+        log.info("bottomAlready: {}", request.getBottomAlready());
 
-            DressRoomAIRequest request = DressRoomAIRequest.builder()
-                .dressRoomId(savedDressRoom.getId())
-                .modelId(modelId)
-                .productTopId(productTopId)
-                .topAlready(topAlready)
-                .productBottomId(productBottomId)
-                .bottomAlready(bottomAlready)
-                .build();
+        webClient.post()
+            .uri(uriBuilder -> uriBuilder.path("/api/fitmeai").build())
+            .contentType(APPLICATION_JSON)
+            .body(BodyInserters.fromValue(request))
+            .retrieve()
+            .onStatus(HttpStatusCode::is5xxServerError, clientResponse ->
+                Mono.error(new InferenceFailureException("AI 서버 오류가 발생했습니다.")))
+            .bodyToMono(byte[].class)
+            .doOnNext(bytes -> {
+                try {
+                    if (bytes == null) {
+                        throw new InferenceFailureException("AI 서버 오류가 발생했습니다.");
+                    }
 
-            log.info("dressRoomId: {}", request.getDressRoomId());
-            log.info("modelId: {}", request.getModelId());
-            log.info("productTopId: {}", request.getProductTopId());
-            log.info("topAlready: {}", request.getTopAlready());
-            log.info("productBottomId: {}", request.getProductBottomId());
-            log.info("bottomAlready: {}", request.getBottomAlready());
+                    String filename = imgUrl;
 
-            webClient.post()
-                .uri(uriBuilder -> uriBuilder.path("/api/fitmeai").build())
-                .contentType(APPLICATION_JSON)
-                .body(BodyInserters.fromValue(request))
-                .retrieve()
-                .onStatus(HttpStatusCode::is5xxServerError, clientResponse ->
-                    Mono.error(new InferenceFailureException("AI 서버 오류가 발생했습니다.")))
-                .bodyToMono(byte[].class)
-                .doOnNext(bytes -> {
-                    try {
-                        if (bytes == null) {
-                            throw new InferenceFailureException("AI 서버 오류가 발생했습니다.");
-                        }
+                    // .images/dressroom 폴더가 없으면 생성
+                    Path path = Paths.get(filename);
+                    if (!Files.exists(path)) {
+                        Files.createDirectories(path);
+                    }
 
-                        String filename = imgUrl;
+                    // 상의와 하의가 모두 있는 경우
+                    if (productTopId != null && productBottomId != null) {
+                        // sum 폴더가 없으면 생성
+                        filename += "sum/";
 
-                        // .images/dressroom 폴더가 없으면 생성
-                        Path path = Paths.get(filename);
+                        path = Paths.get(filename);
                         if (!Files.exists(path)) {
                             Files.createDirectories(path);
                         }
 
-                        // 상의와 하의가 모두 있는 경우
-                        if (productTopId != null && productBottomId != null) {
-                            // sum 폴더가 없으면 생성
-                            filename += "sum/";
+                        filename += productTopId + "_" + productBottomId + ".jpg";
+                    }
+                    // 상의만 있는 경우
+                    else if (productTopId != null) {
+                        // top 폴더가 없으면 생성
+                        filename += "top/";
 
-                            path = Paths.get(filename);
-                            if (!Files.exists(path)) {
-                                Files.createDirectories(path);
-                            }
-
-                            filename += productTopId + "_" + productBottomId + ".jpg";
-                        }
-                        // 상의만 있는 경우
-                        else if (productTopId != null) {
-                            // top 폴더가 없으면 생성
-                            filename += "top/";
-
-                            path = Paths.get(filename);
-                            if (!Files.exists(path)) {
-                                Files.createDirectories(path);
-                            }
-
-                            filename += productTopId + ".jpg";
-                        }
-                        // 하의만 있는 경우
-                        else if (productBottomId != null) {
-                            // bottom 폴더가 없으면 생성
-                            filename += "bottom/";
-
-                            path = Paths.get(filename);
-                            if (!Files.exists(path)) {
-                                Files.createDirectories(path);
-                            }
-
-                            filename += productBottomId + ".jpg";
+                        path = Paths.get(filename);
+                        if (!Files.exists(path)) {
+                            Files.createDirectories(path);
                         }
 
-                        try (FileOutputStream fos = new FileOutputStream(filename)) {
-                            fos.write(bytes);
-                        } catch (IOException e) {
-                            log.error("이미지 저장 중 오류 발생", e);
-                            throw new FileSaveException();
+                        filename += productTopId + ".jpg";
+                    }
+                    // 하의만 있는 경우
+                    else if (productBottomId != null) {
+                        // bottom 폴더가 없으면 생성
+                        filename += "bottom/";
+
+                        path = Paths.get(filename);
+                        if (!Files.exists(path)) {
+                            Files.createDirectories(path);
                         }
+
+                        filename += productBottomId + ".jpg";
+                    }
+
+                    try (FileOutputStream fos = new FileOutputStream(filename)) {
+                        fos.write(bytes);
                     } catch (IOException e) {
-                        log.error("폴더 생성 중 중 오류 발생", e);
+                        log.error("이미지 저장 중 오류 발생", e);
                         throw new FileSaveException();
                     }
-                })
-                .block();
-        }
-
-        return DressRoomResponse.of(savedDressRoom);
+                } catch (IOException e) {
+                    log.error("폴더 생성 중 중 오류 발생", e);
+                    throw new FileSaveException();
+                }
+            })
+            .block();
     }
 
     // DressRoom 삭제
